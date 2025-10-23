d682de510e884420d463a2a37942cc18a559363e
nandhinisathish <39171961+nandhinisathish@users.noreply.github.com>
2025-10-23 17:56:06 -0400

    chnages

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,8 +2,6 @@ import { Router } from "express";
 import { check } from "express-validator";
 import userCTRL from "../controllers/userController.js";
 
-
-
 const router = Router();
 
 // Register User Route
