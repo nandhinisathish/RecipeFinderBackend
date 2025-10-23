f8b11c00f29cac0fcde26d6b82bd7ea8b1c67ec1
nandhinisathish <39171961+nandhinisathish@users.noreply.github.com>
2025-10-23 17:49:42 -0400

    added route 3

---
+++ routes/userRoutes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { check } from "express-validator";
+import userCTRL from "../controllers/userController.js";
+
+
+
+const router = Router();
+
+// Register User Route
+
+
+export default router;
